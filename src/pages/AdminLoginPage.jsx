815ceea91a77effdc641991f527c2b75932fb8aa
MuhittinUlker <muhittin.ulker@hotmail.com>
2024-02-14 14:12:49 +0300

    loginPages updated

--- src/pages/AdminLoginPage.jsx
+++ src/pages/AdminLoginPage.jsx
@@ -1,24 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import "./AdminLoginPage.css";
+import axios from 'axios';
 
 const AdminLoginPage = () => {
-  const [email, setemail] = useState('');
+  const [identity, setIdentity] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
-  const handleLogin = () => {
-    // Burada kullanıcı adı ve şifre kontrolü yapılabilir
-    if (email === 'admin' && password === 'password') {
-      // Giriş başarılı
-      alert('Giriş başarılı!');
-    } else {
-      // Giriş başarısız
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:9090/api/v1/auth/login',{identity,password})
+      const data = response.data
+      console.log(data);
+
+    } catch (error) {
       setErrorMessage('Kullanıcı adı veya şifre yanlış!');
     }
   };
-
-
-
 
   // LOGIN INPUT FONSIYONU 
   useEffect(() => {
@@ -33,31 +31,33 @@ const AdminLoginPage = () => {
 
 
   return (
-    <div className="container">
-      <h2>Admin Login</h2>
-      <form>
+    <div className="admin-login-major-container">
+      <div className="container">
+        <h2>Admin Login</h2>
+        <form onSubmit={handleLogin}>
+          <div className="form-control">
+        <input
+          type="text"
+          required
+          value={identity}
+          onChange={(e) => setIdentity(e.target.value)}
+      
+        />
+        <label>Identity</label>
+        </div>
         <div className="form-control">
-      <input
-        type="text"
-        required
-        value={email}
-        onChange={(e) => setemail(e.target.value)}
-        
-      />
-      <label>Email</label>
+        <input
+          type="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <label>Password</label>
+        </div>
+        <button type='submit' className="btn">Giriş</button>
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
+        </form>
       </div>
-      <div className="form-control">
-      <input
-        type="password"
-        required
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <label>Password</label>
-      </div>
-      <button className="btn" onClick={handleLogin}>Giriş</button>
-      {errorMessage && <div className="error-message">{errorMessage}</div>}
-      </form>
     </div>
   );
 };
