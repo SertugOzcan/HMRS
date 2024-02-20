a233e016826563d3e43a6b4f3740e34d77925290
LENOVO <sertugozcan@gmail.com>
2024-02-20 16:03:56 +0300

    companyformupdate

--- src/pages/UpdateCompanyForTheFirstTimePage.jsx
+++ src/pages/UpdateCompanyForTheFirstTimePage.jsx
@@ -2,22 +2,20 @@ import axios from "axios";
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { SupervisorPageAPIContext } from "../context/SupervisorPageAPIContext";
-import { AuthContext } from "../context/AuthContext"
-import './UpdatedCompanyForTheFirstTimePage.css'
+import { AuthContext } from "../context/AuthContext";
+import "./UpdatedCompanyForTheFirstTimePage.css";
 
 const UpdateCompanyForTheFirstTimePage = () => {
-
-  const {companyData} = useContext(SupervisorPageAPIContext);
-  const {isAuthenticated} = useContext(AuthContext);
-
+  const { companyData } = useContext(SupervisorPageAPIContext);
+  const { isAuthenticated } = useContext(AuthContext);
 
   const [establishmentDate, setEstablishmentDate] = useState("");
   const [companyLogo, setCompanyLogo] = useState("");
   const [companyAddress, setCompanyAddress] = useState("");
   const [hrInfoList, setHrInfoList] = useState([]);
-  const [companyDepartments,setCompanyDepartments] = useState([]);
-  const [companyHolidays,setCompanyHolidays] = useState([]);
-  const [companyIncomes , setCompanyIncomes] = useState([]);
+  const [companyDepartments, setCompanyDepartments] = useState([]);
+  const [companyHolidays, setCompanyHolidays] = useState([]);
+  const [companyIncomes, setCompanyIncomes] = useState([]);
   const [companyExpenses, setCompanyExpenses] = useState([]);
 
   const [hrInfoName, setHRInfoName] = useState("");
@@ -25,17 +23,17 @@ const UpdateCompanyForTheFirstTimePage = () => {
   const [hrInfoEmail, setHRInfoEmail] = useState("");
   const [hrInfoPhone, setHRInfoPhone] = useState("");
 
-  const [departmentName,setDepartmentName] = useState('');
-  const [departmentShifts,setDepartmentShifts] = useState('');
-  const [departmentBreaks,setDepartmentBreaks] = useState('');
+  const [departmentName, setDepartmentName] = useState("");
+  const [departmentShifts, setDepartmentShifts] = useState("");
+  const [departmentBreaks, setDepartmentBreaks] = useState("");
 
-  const [holidayName,setHolidayName] = useState('');
-  const [holidayDuration,setHolidayDuration] = useState(0);
+  const [holidayName, setHolidayName] = useState("");
+  const [holidayDuration, setHolidayDuration] = useState(0);
 
-  const [incomeDescription, setIncomeDescription] = useState('');
+  const [incomeDescription, setIncomeDescription] = useState("");
   const [incomeAmount, setIncomeAmount] = useState(0);
 
-  const [expenseDescription, setExpenseDescription] = useState('');
+  const [expenseDescription, setExpenseDescription] = useState("");
   const [expenseAmount, setExpenseAmount] = useState(0);
 
   const handleAddHRInfo = (e) => {
@@ -50,57 +48,61 @@ const UpdateCompanyForTheFirstTimePage = () => {
       },
     ]);
     console.log(hrInfoList);
-    setHRInfoName('');
-    setHRInfoSurname('');
-    setHRInfoEmail('');
-    setHRInfoPhone('');
+    setHRInfoName("");
+    setHRInfoSurname("");
+    setHRInfoEmail("");
+    setHRInfoPhone("");
   };
   const handleAddDepartment = (e) => {
     e.preventDefault();
-    setCompanyDepartments((prevDepartments)=>[
-        ...prevDepartments, {
-            name: departmentName,
-            shifts: departmentShifts,
-            breaks: departmentBreaks
-        }
+    setCompanyDepartments((prevDepartments) => [
+      ...prevDepartments,
+      {
+        name: departmentName,
+        shifts: departmentShifts,
+        breaks: departmentBreaks,
+      },
     ]);
-    setDepartmentName('');
-    setDepartmentShifts('');
-    setDepartmentBreaks('');
-  }
+    setDepartmentName("");
+    setDepartmentShifts("");
+    setDepartmentBreaks("");
+  };
   const handleAddHoliday = (e) => {
     e.preventDefault();
-    setCompanyHolidays((prevHolidays)=>[
-        ...prevHolidays,{
-            name: holidayName,
-            duration: holidayDuration
-        }
+    setCompanyHolidays((prevHolidays) => [
+      ...prevHolidays,
+      {
+        name: holidayName,
+        duration: holidayDuration,
+      },
     ]);
-    setHolidayName('');
-    setHolidayDuration('');
-  }
+    setHolidayName("");
+    setHolidayDuration("");
+  };
   const handleAddIncome = (e) => {
     e.preventDefault();
-    setCompanyIncomes((prevIncomes)=> [
-        ...prevIncomes,{
-            description: incomeDescription,
-            amount: incomeAmount
-        }
+    setCompanyIncomes((prevIncomes) => [
+      ...prevIncomes,
+      {
+        description: incomeDescription,
+        amount: incomeAmount,
+      },
     ]);
-    setIncomeDescription('');
+    setIncomeDescription("");
     setIncomeAmount(0);
-  }
+  };
   const handleAddExpense = (e) => {
     e.preventDefault();
-    setCompanyExpenses((prevExpenses)=> [
-        ...prevExpenses,{
-            description: expenseDescription,
-            amount: expenseAmount
-        }
+    setCompanyExpenses((prevExpenses) => [
+      ...prevExpenses,
+      {
+        description: expenseDescription,
+        amount: expenseAmount,
+      },
     ]);
-    setExpenseDescription('');
+    setExpenseDescription("");
     setExpenseAmount(0);
-  }
+  };
   const navigate = useNavigate();
 
   const handleSubmit = async () => {
@@ -113,13 +115,16 @@ const UpdateCompanyForTheFirstTimePage = () => {
       departments: companyDepartments,
       holidays: companyHolidays,
       incomes: companyIncomes,
-      expenses: companyExpenses
+      expenses: companyExpenses,
     };
     console.log(payload);
-    const response = await axios.put('http://localhost:9095/api/v1/company/update-for-first-time', payload)
+    const response = await axios.put(
+      "http://localhost:9095/api/v1/company/update-for-first-time",
+      payload
+    );
     console.log(response);
-    if(response === true){
-        navigate("/yonetici-page");
+    if (response === true) {
+      navigate("/yonetici-page");
     }
   };
 
@@ -158,8 +163,9 @@ const UpdateCompanyForTheFirstTimePage = () => {
       <div className="hr-info-list-container">
         <div className="hr-info-container">
           <div className="hr-info-name">
-            <label>Name</label>
             <input
+              className="input"
+              placeholder="Name"
               type="text"
               value={hrInfoName}
               onChange={(event) => setHRInfoName(event.target.value)}
@@ -167,8 +173,9 @@ const UpdateCompanyForTheFirstTimePage = () => {
             />
           </div>
           <div className="hr-info-surname">
-            <label>Surname</label>
             <input
+              className="input"
+              placeholder="Surname"
               type="text"
               value={hrInfoSurname}
               onChange={(event) => setHRInfoSurname(event.target.value)}
@@ -176,8 +183,9 @@ const UpdateCompanyForTheFirstTimePage = () => {
             />
           </div>
           <div className="hr-info-email">
-            <label>Email</label>
             <input
+              className="input"
+              placeholder="Email"
               type="text"
               value={hrInfoEmail}
               onChange={(event) => setHRInfoEmail(event.target.value)}
@@ -185,8 +193,9 @@ const UpdateCompanyForTheFirstTimePage = () => {
             />
           </div>
           <div className="hr-info-phone">
-            <label>Phone</label>
             <input
+              className="input"
+              placeholder="Phone"
               type="text"
               value={hrInfoPhone}
               onChange={(event) => setHRInfoPhone(event.target.value)}
@@ -202,8 +211,9 @@ const UpdateCompanyForTheFirstTimePage = () => {
       <div className="company-department-list-container">
         <div className="company-department-container">
           <div className="department-name">
-            <label>Department Name:</label>
             <input
+              className="input"
+              placeholder="Department Name"
               type="text"
               value={departmentName}
               onChange={(event) => setDepartmentName(event.target.value)}
@@ -211,8 +221,9 @@ const UpdateCompanyForTheFirstTimePage = () => {
             />
           </div>
           <div className="department-shifts">
-            <label>Shifts:</label>
             <input
+              className="input"
+              placeholder="Shifts"
               type="text"
               value={departmentShifts}
               onChange={(event) => setDepartmentShifts(event.target.value)}
@@ -220,8 +231,9 @@ const UpdateCompanyForTheFirstTimePage = () => {
             />
           </div>
           <div className="department-breaks">
-            <label>Breaks:</label>
             <input
+              className="input"
+              placeholder="Breaks"
               type="text"
               value={departmentBreaks}
               onChange={(event) => setDepartmentBreaks(event.target.value)}
@@ -235,14 +247,26 @@ const UpdateCompanyForTheFirstTimePage = () => {
 
       <div className="company-holiday-list-container">
         <div className="company-holiday-container">
-            <div className="holiday-name">
-                <label>Holiday Name:</label>
-                <input type="text" value={holidayName} onChange={(event)=>setHolidayName(event.target.value)} required/>
-            </div>
-            <div className="holiday-duration">
-                <label>Holiday Duration:</label>
-                <input type="number" value={holidayDuration} onChange={(event)=>setHolidayDuration(event.target.value)} required/>
-            </div>
+          <div className="holiday-name">
+            <input
+              className="input"
+              placeholder="Holiday Name"
+              type="text"
+              value={holidayName}
+              onChange={(event) => setHolidayName(event.target.value)}
+              required
+            />
+          </div>
+          <div className="holiday-duration">
+            <label>Holiday Duration:</label>
+            <input
+              className="input"
+              type="number"
+              value={holidayDuration}
+              onChange={(event) => setHolidayDuration(event.target.value)}
+              required
+            />
+          </div>
         </div>
         <button onClick={handleAddHoliday}>Add Holiday</button>
         {/* <textarea name="company-holidays" id="company-holidays" cols="30" rows="10" value={JSON.stringify(companyHolidays)} readOnly></textarea> */}
@@ -250,14 +274,27 @@ const UpdateCompanyForTheFirstTimePage = () => {
 
       <div className="company-income-list-container">
         <div className="company-income-container">
-            <div className="income-description">
-                <label>Income Description:</label>
-                <input type="text" value={incomeDescription} onChange={(event)=>setIncomeDescription(event.target.value)} required/>
-            </div>
-            <div className="income-amount">
-                <label>Income Amount:</label>
-                <input type="number" value={incomeAmount} onChange={(event)=>setIncomeAmount(event.target.value)} required/>
-            </div>
+          <div className="income-description">
+            <input
+              className="input"
+              placeholder="Income Description"
+              type="text"
+              value={incomeDescription}
+              onChange={(event) => setIncomeDescription(event.target.value)}
+              required
+            />
+          </div>
+          <div className="income-amount">
+            <label>Income Amount:</label>
+            <input
+              className="input"
+              placeholder="Income Amount"
+              type="number"
+              value={incomeAmount}
+              onChange={(event) => setIncomeAmount(event.target.value)}
+              required
+            />
+          </div>
         </div>
         <button onClick={handleAddIncome}>Add Income</button>
         {/* <textarea name="company-incomes" id="company-incomes" cols="30" rows="10" value={JSON.stringify(companyIncomes)} readOnly></textarea> */}
@@ -265,14 +302,27 @@ const UpdateCompanyForTheFirstTimePage = () => {
 
       <div className="company-expense-list-container">
         <div className="company-expense-container">
-            <div className="expense-description">
-                <label>Expense Description:</label>
-                <input type="text" value={expenseDescription} onChange={(event)=>setExpenseDescription(event.target.value)} required/>
-            </div>
-            <div className="expense-amount">
-                <label>Expense Amount:</label>
-                <input type="number" value={expenseAmount} onChange={(event)=>setExpenseAmount(event.target.value)} required/>
-            </div>
+          <div className="expense-description">
+            <input
+              className="input"
+              placeholder="Expense Description"
+              type="text"
+              value={expenseDescription}
+              onChange={(event) => setExpenseDescription(event.target.value)}
+              required
+            />
+          </div>
+          <div className="expense-amount">
+            <label>Expense Amount:</label>
+            <input
+              className="input"
+              placeholder="Expense Amount"
+              type="number"
+              value={expenseAmount}
+              onChange={(event) => setExpenseAmount(event.target.value)}
+              required
+            />
+          </div>
         </div>
         <button onClick={handleAddExpense}>Add Expense</button>
         {/* <textarea name="company-expenses" id="company-expenses" cols="30" rows="10" value={JSON.stringify(companyExpenses)} readOnly></textarea> */}
