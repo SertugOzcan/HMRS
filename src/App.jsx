a3e23cff0deaa74029229ba95c99b9cbf18d1307
MuhittinUlker <muhittin.ulker@hotmail.com>
2024-02-22 12:21:47 +0300

    footer & bg img update

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import RegisterPage from "./pages/RegisterPage";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
 import PrivateRoute from "./services/PrivateRoute";
-import {UserPreferencesContext} from "./context/UserPreferencesContext";
+import { UserPreferencesContext } from "./context/UserPreferencesContext";
 import { useContext } from "react";
 import { AdminPageAPIContextProvider } from "./context/AdminPageAPIContext";
 import { AuthContext } from "./context/AuthContext";
@@ -17,113 +17,112 @@ import { GuestPageAPIContextProvider } from "./context/GuestPageAPIContext";
 import { PersonnelPageAPIContextProvider } from "./context/PersonalPageAPIContext";
 import HomePageSideBar from "./components/HomePageSideBar/HomePageSideBar";
 import NavBar from "./components/NavBar/NavBar";
-import RegisteredUsers from './components/AdminComponents/RegisteredUsers/RegisteredUsers'
-import ManagerRequests from './components/AdminComponents/ManagerRequests/ManagerRequests'
-import CommentRequests from './components/AdminComponents/CommentRequests/CommentRequests'
+import RegisteredUsers from "./components/AdminComponents/RegisteredUsers/RegisteredUsers";
+import ManagerRequests from "./components/AdminComponents/ManagerRequests/ManagerRequests";
+import CommentRequests from "./components/AdminComponents/CommentRequests/CommentRequests";
 import NoPage from "./pages/NoPage";
 
 function App() {
   const { theme } = useContext(UserPreferencesContext);
-  const {isAuthenticated} = useContext(AuthContext);
-  
+  const { isAuthenticated } = useContext(AuthContext);
+
   return (
-    
-        <Router>
-          <div className={`app-container ${theme}`}>
-          {isAuthenticated && <HomePageSideBar />}
-            <NavBar theme={`${theme}`}/>
-            <Footer />
-            <Routes>
-              <Route path="/" element={<LoginPage />} />
-              <Route path="/admin-login" element={<AdminLoginPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route
-                path="/admin-page"
+    <Router>
+      <div className={`app-container ${theme}`}>
+        {isAuthenticated && <HomePageSideBar />}
+        <NavBar theme={`${theme}`} />
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/admin-login" element={<AdminLoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/admin-page"
+            element={
+              <PrivateRoute
                 element={
-                  <PrivateRoute
-                    element={
-                      <AdminPageAPIContextProvider>
-                        <RegisteredUsers />
-                      </AdminPageAPIContextProvider>
-                    }
-                  />
-                }
-              /> {/* Volkan: bu üstteki route'u silip, AdminLoginPage'de navigationı "/registered-users" yapınca hiçbirşey farketmiyor bu arada, haber olsun */}
-              <Route
-                path="/admin-page/registered-users"
-                element={
-                  <PrivateRoute
-                    element={
-                      <AdminPageAPIContextProvider>
-                        <RegisteredUsers />
-                      </AdminPageAPIContextProvider>
-                    }
-                  />
+                  <AdminPageAPIContextProvider>
+                    <RegisteredUsers />
+                  </AdminPageAPIContextProvider>
                 }
               />
-              <Route
-                path="/admin-page/manager-requests"
+            }
+          />{" "}
+          {/* Volkan: bu üstteki route'u silip, AdminLoginPage'de navigationı "/registered-users" yapınca hiçbirşey farketmiyor bu arada, haber olsun */}
+          <Route
+            path="/admin-page/registered-users"
+            element={
+              <PrivateRoute
                 element={
-                  <PrivateRoute
-                    element={
-                      <AdminPageAPIContextProvider>
-                        <ManagerRequests />
-                      </AdminPageAPIContextProvider>
-                    }
-                  />
+                  <AdminPageAPIContextProvider>
+                    <RegisteredUsers />
+                  </AdminPageAPIContextProvider>
                 }
               />
-              <Route
-                path="/admin-page/comment-requests"
+            }
+          />
+          <Route
+            path="/admin-page/manager-requests"
+            element={
+              <PrivateRoute
                 element={
-                  <PrivateRoute
-                    element={
-                      <AdminPageAPIContextProvider>
-                        <CommentRequests />
-                      </AdminPageAPIContextProvider>
-                    }
-                  />
+                  <AdminPageAPIContextProvider>
+                    <ManagerRequests />
+                  </AdminPageAPIContextProvider>
                 }
               />
-              <Route
-                path="/yonetici-page"
+            }
+          />
+          <Route
+            path="/admin-page/comment-requests"
+            element={
+              <PrivateRoute
                 element={
-                  <PrivateRoute
-                    element={
-                      <SupervisorPageAPIContextProvider>
-                        <YoneticiPage />
-                      </SupervisorPageAPIContextProvider>
-                    }
-                  />
+                  <AdminPageAPIContextProvider>
+                    <CommentRequests />
+                  </AdminPageAPIContextProvider>
                 }
               />
-              {/*<Route path="/ziyaretci-page" element={<PrivateRoute element={<ZiyaretciPage />}/>} />*/}
-              <Route
-                path="/ziyaretci-page"
+            }
+          />
+          <Route
+            path="/yonetici-page"
+            element={
+              <PrivateRoute
                 element={
-                  <GuestPageAPIContextProvider>
-                    <ZiyaretciPage />
-                  </GuestPageAPIContextProvider>
+                  <SupervisorPageAPIContextProvider>
+                    <YoneticiPage />
+                  </SupervisorPageAPIContextProvider>
                 }
               />
-              <Route
-                path="/personel-page"
+            }
+          />
+          {/*<Route path="/ziyaretci-page" element={<PrivateRoute element={<ZiyaretciPage />}/>} />*/}
+          <Route
+            path="/ziyaretci-page"
+            element={
+              <GuestPageAPIContextProvider>
+                <ZiyaretciPage />
+              </GuestPageAPIContextProvider>
+            }
+          />
+          <Route
+            path="/personel-page"
+            element={
+              <PrivateRoute
                 element={
-                  <PrivateRoute
-                    element={
-                      <PersonnelPageAPIContextProvider>
-                        <PersonelPage />
-                      </PersonnelPageAPIContextProvider>
-                    }
-                  />
+                  <PersonnelPageAPIContextProvider>
+                    <PersonelPage />
+                  </PersonnelPageAPIContextProvider>
                 }
               />
-              <Route path="/register-page" element={<RegisterPage />} />
-              <Route path="*" element={<NoPage />} />
-            </Routes>
-          </div>
-        </Router>
-      
+            }
+          />
+          <Route path="/register-page" element={<RegisterPage />} />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
