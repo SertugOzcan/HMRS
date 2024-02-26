9d025b6d06d24d3cfd06c4b275789d71bcdfb477
LENOVO <sertugozcan@gmail.com>
2024-02-27 02:14:55 +0300

    admin manager css update

--- src/components/AdminComponents/RegisteredUsers/RegisteredUsers.jsx
+++ src/components/AdminComponents/RegisteredUsers/RegisteredUsers.jsx
@@ -1,25 +1,36 @@
-import React from 'react'
+import React from "react";
 import { useContext } from "react";
 import { AdminPageAPIContext } from "../../../context/AdminPageAPIContext";
-import './RegisteredUsers.css'   
-         
+import "./RegisteredUsers.css";
+
 const RegisteredUsers = () => {
-    const {activeUsers} = useContext(AdminPageAPIContext);
+  const { activeUsers } = useContext(AdminPageAPIContext);
   return (
     <div className="registered-users-container">
-        <h2>Active Users</h2>
-        <div className="registered-users">
-          {activeUsers.map((user) => (
-            <div key={user.email} className="registered-user">
-              <p><b>Email:</b> {user.email}</p>
-              <p><b>Phone:</b> {user.phone}</p>
-              <p><b>Password:</b> {user.password}</p>
-              <p><b>Role:</b> {user.role}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-  )
-}
+      <h2>Active Users</h2>
 
-export default RegisteredUsers
+      <div className="registered-users">
+        {activeUsers.map((user) => (
+          <div key={user.email} className="card">
+            <div className="registered-user">
+              <p>
+                <b>Email:</b> {user.email}
+              </p><br />
+              <p>
+                <b>Phone:</b> {user.phone}
+              </p><br />
+              <p>
+                <b>Password:</b> {user.password}
+              </p><br />
+              <p>
+                <b>Role:</b> {user.role}
+              </p><br />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RegisteredUsers;
