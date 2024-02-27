e62c1894855fe237a5d1c3b1884ead41ffdbb1c9
alivolkansahin <avolkan.shn@gmail.com>
2024-02-27 21:52:45 +0300

    Personnel Page advance,dayoff and spending contexts have been created

---
+++ src/context/PersonnelPageSpendingAPIContext.jsx
@@ -0,0 +1,93 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "./AuthContext";
+
+export const PersonnelPageSpendingAPIContext = createContext();
+
+// eslint-disable-next-line react/prop-types
+export const PersonnelPageSpendingAPIContextProvider = ({children}) => {
+
+    const [pendingSpendingRequests, setPendingSpendingRequests] = useState([]);
+    const [notPendingSpendingRequests, setNotPendingSpendingRequests] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const {isAuthenticated} = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(isAuthenticated.role!=="PERSONNEL"){
+            return navigate("/login")
+        }
+        setIsLoading(true);
+        const getRequests = async () => {
+            try {
+                const response = await axios.get(`http://localhost:9087/api/v1/spending/get-all-my-requests/${isAuthenticated.token}`)
+                console.log("SPENDINGREQUESTS-DATA: ", response.data)
+                setPendingSpendingRequests(response.data.filter(request => request.requestStatus === "PENDING"))
+                setNotPendingSpendingRequests(response.data.filter(request => request.requestStatus !== "PENDING"));
+            } catch (error) {
+                console.error("Error while fetching the spending requests data:", error);
+            } finally {
+                // console.log("PENDING OLAN DAYOFFLAR: ", pendingDayOffRequests);
+                // console.log("PENDING OLMAYAN DAYOFFLAR: ", notPendingDayOffRequests);
+                setIsLoading(false);
+            }
+        };
+        getRequests();
+    }, []);
+
+    const handleSubmit = async (newRequest) => {
+        setIsLoading(true);
+        const formData = new FormData();
+        // list multipart attachments hata verirse buraya bi bak -- Volkan kendine not...
+        for (const key in newRequest) {
+            formData.append(key, newRequest[key]);
+        }
+        formData.append("token", isAuthenticated.token);
+        console.log("HAZIRLANAN NEW SPENDING FORM DATA: ", formData);
+        try {
+            const response = await axios.post("http://localhost:9087/api/v1/spending/create-request", formData, {
+                headers: {
+                  "Content-Type": "multipart/form-data",
+                },
+            });
+            if (response.status === 200) {
+                // window.location.reload(true);
+            }    
+        } catch (error) {
+            console.log("Error on creating new spending request! ", error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    const handleCancelRequest = async (requestId) => {
+        setIsLoading(true);
+        const payload = {
+            "token": isAuthenticated.token,
+            "requestId": requestId
+        }
+        console.log("Hazırlanan cancel spending payload:", payload);
+        try {
+            const response = await axios.patch("http://localhost:9087/api/v1/spending/cancel-request", payload)
+            if (response.status === 200) {
+                // window.location.reload(true);
+            }    
+        } catch (error) {
+            console.log("Error on cancelling spending request! ", error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    return (
+        <PersonnelPageSpendingAPIContext.Provider value={{pendingSpendingRequests, notPendingSpendingRequests, handleSubmit, handleCancelRequest}}>
+            {isLoading ? (
+                <h1 className="loading-h1-tags">Loading...</h1>
+            ) : (
+                children
+            )}
+        </PersonnelPageSpendingAPIContext.Provider>
+    )
+
+}
