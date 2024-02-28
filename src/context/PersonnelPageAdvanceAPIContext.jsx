61ffd3ed912ae21514b16726b52fb3cc0b3edd91
LENOVO <sertugozcan@gmail.com>
2024-02-28 15:30:35 +0300

    loader added

--- src/context/PersonnelPageAdvanceAPIContext.jsx
+++ src/context/PersonnelPageAdvanceAPIContext.jsx
@@ -73,7 +73,35 @@ export const PersonnelPageAdvanceAPIContextProvider = ({children}) => {
     return (
         <PersonnelPageAdvanceAPIContext.Provider value={{advanceRequests, handleSubmit, handleCancelRequest}}>
             {isLoading ? (
-                <h1 className="loading-h1-tags">Loading...</h1>
+                <div className="loader">
+                <div className="box box0">
+                  <div></div>
+                </div>
+                <div className="box box1">
+                  <div></div>
+                </div>
+                <div className="box box2">
+                  <div></div>
+                </div>
+                <div className="box box3">
+                  <div></div>
+                </div>
+                <div className="box box4">
+                  <div></div>
+                </div>
+                <div className="box box5">
+                  <div></div>
+                </div>
+                <div className="box box6">
+                  <div></div>
+                </div>
+                <div className="box box7">
+                  <div></div>
+                </div>
+                <div className="ground">
+                  <div></div>
+                </div>
+              </div>
             ) : (
                 children
             )}
