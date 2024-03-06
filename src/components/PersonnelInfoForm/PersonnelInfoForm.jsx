a7960bfae1018b39686409a39fb20030d3b67b9f
MuhittinUlker <muhittin.ulker@hotmail.com>
2024-03-06 15:10:15 +0300

    small fixes

--- src/components/PersonnelInfoForm/PersonnelInfoForm.jsx
+++ src/components/PersonnelInfoForm/PersonnelInfoForm.jsx
@@ -13,12 +13,15 @@ const PersonnelInfoForm = () => {
   const [holidays, setHolidays] = useState([]);
 
   useEffect(() => {
-    const modifiedHolidays = personnel.companyHolidays.map(holiday => {
+    const modifiedHolidays = personnel.companyHolidays.map((holiday) => {
       const currentEndDate = new Date(holiday.endTime);
       const currentEndDateIncluded = new Date(currentEndDate);
       currentEndDateIncluded.setDate(currentEndDate.getDate() + 1);
-      return {...holiday, endTime: currentEndDateIncluded.toISOString().split('T')[0]};
-    })
+      return {
+        ...holiday,
+        endTime: currentEndDateIncluded.toISOString().split("T")[0],
+      };
+    });
 
     setHolidays(modifiedHolidays);
   }, []);
@@ -28,7 +31,8 @@ const PersonnelInfoForm = () => {
     setIsEditInfoClicked(true);
   };
 
-  return (<>
+  return (
+    <>
       <main className="personnel-page-main" id="main">
         <section className="personnel-page-main-section">
           <h1>
@@ -42,107 +46,111 @@ const PersonnelInfoForm = () => {
           </h4>
         </section>
       </main>
-    <div className="personnel-page-container">
-      <div className="personnel-container">
-        <div className="personnel-photo">
-          <img src={personnel.image}></img>
-        </div>
-        <div className="personnel-info">
-          <p>
-            <strong>Name:</strong> {personnel.name} {personnel.lastName}
-          </p>
-          <p>
-            <strong>Email:</strong> {personnel.email}
-          </p>
-          <p>
-            <strong>Phone:</strong> {personnel.phones[0].phoneType}
-            {": "}
-            {personnel.phones[0].phoneNumber}
-          </p>
-              <p>
-                <strong>Salary:</strong> {personnel.salary}
-              </p>
-          <p>
-            <strong>Department:</strong> {personnel.department.name}
-          </p>
-          {isAuthenticated.role === "PERSONNEL" && (
-            <>
-              <p>
-                <strong>Shift:</strong> {personnel.department.shiftHour}
-              </p>
-              <p>
-                <strong>Break:</strong> {personnel.department.breakHour}
-              </p>
-              <p>
-                <strong>DayOff Quota:</strong> {personnel.dayOff}
-              </p>
-            </>
-          )}
-          <div className="btn-container">
-            <button
-              className="edit-info-button"
-              onClick={(e) => handleEditInfoClick(e)}
+      <div className="personnel-page-container">
+        <div className="personnel-container">
+          <div className="personnel-photo">
+            <img src={personnel.image}></img>
+          </div>
+          <div className="personnel-info">
+            <p>
+              <strong>Name:</strong> {personnel.name} {personnel.lastName}
+            </p>
+            <p>
+              <strong>Email:</strong> {personnel.email}
+            </p>
+            <p>
+              <strong>Phone:</strong> {personnel.phones[0].phoneType}
+              {": "}
+              {personnel.phones[0].phoneNumber}
+            </p>
+            <p>
+              <strong>Salary:</strong> {personnel.salary}
+            </p>
+            <p>
+              <strong>Department:</strong> {personnel.department.name}
+            </p>
+            {isAuthenticated.role === "PERSONNEL" && (
+              <>
+                <p>
+                  <strong>Shift:</strong> {personnel.department.shiftHour}
+                </p>
+                <p>
+                  <strong>Break:</strong> {personnel.department.breakHour}
+                </p>
+                <p>
+                  <strong>DayOff Quota:</strong> {personnel.dayOff}
+                </p>
+              </>
+            )}
+            <div className="btn-container">
+              <button
+                className="edit-info-button"
+                onClick={(e) => handleEditInfoClick(e)}
               >
-              Edit My Info
-            </button>
-            {isEditInfoClicked && (
-              <div
-              className="edit-info-background"
-              onClick={() => setIsEditInfoClicked(false)}
-              >
+                Edit My Info
+              </button>
+              {isEditInfoClicked && (
                 <div
-                  className="edit-info-content"
-                  onClick={(e) => e.stopPropagation()}
+                  className="edit-info-background"
+                  onClick={() => setIsEditInfoClicked(false)}
+                >
+                  <div
+                    className="edit-info-content"
+                    onClick={(e) => e.stopPropagation()}
                   >
-                  <EditMyInfoForm />
+                    <EditMyInfoForm />
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+          <div className="personnel-company-info-container">
+            <div className="personnel-company-summary">
+              <div className="personnel-company-img-and-company-name">
+                <img src={personnel.companyLogo} />
+                <div className="personnel-company-name">
+                  <strong>Company Name:</strong>
+                  <p>{personnel.companyName}</p>
                 </div>
               </div>
-            )}
-          </div>
-        </div>
-        <div className="personnel-company-info-container">
-          <div className="personnel-company-summary">
-            <div className="personnel-company-img-and-company-name">
-              <img src={personnel.companyLogo} />
-              <div className="personnel-company-name">
-                <strong>Company Name:</strong>
-                <p>{personnel.companyName}</p>
-              </div>
-            </div>
-            <div className="personnel-company-data">
-              <div className="personnel-hrinfo-container">
-              <strong>HR Infos:</strong>
-                {personnel.hrInfos.map((hrInfo) => (
-                  <div key={hrInfo.phone}>
-                    <p key={hrInfo.email}>Mail:{hrInfo.email}</p>
-                    <p key={hrInfo.phone}>Phone:{hrInfo.phone}</p>
-                  </div>
-                ))}
+              <div className="personnel-company-data">
+                <div className="personnel-hrinfo-container">
+                  <strong>HR Infos:</strong>
+                  {personnel.hrInfos.map((hrInfo) => (
+                    <div key={hrInfo.phone}>
+                      <p key={hrInfo.email}>Mail:{hrInfo.email}</p>
+                      <p key={hrInfo.phone}>Phone:{hrInfo.phone}</p>
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="calendar-container">
-      <Calendar
-        className="holiday-calendar"
-        value={new Date()}
-        tileContent={({ date }) => {
-          const holiday = holidays.find((holiday) => {
-            const startDate = new Date(holiday.startTime);
-            startDate.setHours(0, 0, 0, 0);
-            const endDate = new Date(holiday.endTime);
-            endDate.setHours(0, 0, 0, 0);
-            return ((date >= startDate) && (date < endDate));
-          });
-          return holiday ? (
-            <div className="holiday-day">{holiday.name}</div>
-            ) : null;
-          }}
+        <div className="calendar-container">
+          <h3>Company Holidays</h3>
+          <Calendar
+            className="holiday-calendar"
+            value={new Date()}
+            tileContent={({ date }) => {
+              const holiday = holidays.find((holiday) => {
+                const startDate = new Date(holiday.startTime);
+                startDate.setHours(0, 0, 0, 0);
+                const endDate = new Date(holiday.endTime);
+                endDate.setHours(0, 0, 0, 0);
+                return date >= startDate && date < endDate;
+              });
+              return holiday ? (
+                <div className="holiday-day-container">
+                <div className="holiday-day">{holiday.name}</div>
+                <div className="hidden-holiday-day">{holiday.name}</div>
+                </div>
+              ) : null;
+            }}
           />
+        </div>
       </div>
-    </div>
-          </>
+    </>
   );
 };
 
